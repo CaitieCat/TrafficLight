78e2d15cd0723f9546ac6850d93ecc614bafa990
Caitie Garrood <caitlinanneg@gmail.com>
2021-05-05 12:34:29 -0400

    Adds text to component

--- src/components/TrafficLight.js
+++ src/components/TrafficLight.js
@@ -1,1 +1,17 @@
 import React from 'react';
+
+export default function TrafficLight(props) {
+    if(props === 'red'){
+        return(
+            <h1>The light is red.</h1>
+        )
+    } else if (props === 'yellow'){
+        return(
+            <h1>The light is yellow.</h1>
+        )
+    } else if (props === 'green'){
+        return(
+            <h1>The light is green.</h1>
+        )
+    }
+}
